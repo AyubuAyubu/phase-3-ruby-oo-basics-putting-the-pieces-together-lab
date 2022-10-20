4ca8df6888aab2c1cf25c545d73c039c8a2564da
AyubuAyubu <ayubu6143@gmail.com>
2022-10-20 10:06:34 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
-
+class Book
+    attr_reader :title
+    attr_accessor :page_count, :author, :genre
+    def initialize(title)
+        @title = title 
+    end
+    def turn_page
+        # puts "can turn the page"
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+book = Book.new('Ruby on Table')
+puts book.title
+book_author  = Book.new('The CodeFather')
+puts book_author.author
 
